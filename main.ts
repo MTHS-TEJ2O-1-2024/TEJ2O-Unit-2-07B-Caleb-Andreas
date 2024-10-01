9a6f211b76904953fc78322c127f652a9f0c0c35
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-01 11:20:34 -0400

    Made a clicker game

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Andreas
+ * Created on: Oct 2024
+ * This program is a clicker game.
 */
 
-basic.showString('Hello, World!')
+//Define variable
+let cookieNumber = 0
+
+// Happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// cookieNumber goes up by 1 when A button pressed
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    cookieNumber = cookieNumber + 1
+    basic.showNumber(cookieNumber)
+})
+
+// Reset cookieNumber when B button is pressed.
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    cookieNumber = 0
+    basic.showNumber(cookieNumber)
+})
