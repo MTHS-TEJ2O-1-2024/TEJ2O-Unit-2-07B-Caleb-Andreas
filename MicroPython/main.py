a16d1bd394526d973c74d3099bc6555e95c1b9ef
Caleb-Andreas <caleb.andreas@mths.ca>
2024-11-01 15:26:12 +0000

    Made cookie clicker program

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,18 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+# variables
+cookieNumber = number = 0
 
-display.scroll("Hello, World!")
+# cookieNumber goes up by one on a button press.
+while True:
+    if button_a.is_pressed():
+        cookieNumber = cookieNumber + 1
+        display.clear()
+        display.scroll(str(cokieNumber))
+
+# reset cookieNumber
+    if button_a.is_pressed():
+        cookieNumber = 0
+        display.clear()
+        display.scroll(str(cokieNumber))
